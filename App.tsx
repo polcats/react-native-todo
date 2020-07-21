2f4ecc93078a466cb5ea1bf846d51cde898d1634
Paul Catalan <24451518+polcats@users.noreply.github.com>
2020-07-21 12:19:26 +0800

    Create initial app.

--- App.tsx
+++ App.tsx
@@ -1,21 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App: React.FC = () => {
+    return (
+        <>
+            <View style={styles.container}>
+                <View style={styles.top}>
+                    <Text style={styles.topText}>To-do List</Text>
+                </View>
+                <View style={styles.middle}>
+                    <Text style={styles.text}>Text</Text>
+                </View>
+            </View>
+        </>
+    );
+};
 
+export default App;
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        flexDirection: "column",
+        backgroundColor: "#000",
+        alignItems: "center",
+        justifyContent: "flex-start",
+        paddingTop: 25,
+    },
+    top: {
+        backgroundColor: "skyblue",
+        width: "100%",
+        height: 50,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    topText: {
+        fontSize: 25,
+        color: "#fff",
+        fontWeight: "bold",
+    },
+    middle: {
+        flex: 1,
+        backgroundColor: "#333",
+        width: "100%",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    text: {
+        color: "#fff",
+    },
 });
